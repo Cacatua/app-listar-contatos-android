5b7bb0732ec72808409ba973719974675e5df0c5
Eduardo Freitas da Silva <edufds1@hotmail.com>
2022-01-31 23:20:05 -0300

    first commit

---
+++ app/src/main/java/com/kktua/contatosbootcamp/Contact.kt
@@ -0,0 +1,3 @@
+package com.kktua.contatosbootcamp
+
+data class Contact(val name: String, val phoneNumber: String)
---
+++ app/src/main/java/com/kktua/contatosbootcamp/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.kktua.contatosbootcamp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CONTACT = 1
+//    val LINEAR_LAYOUT_VERTICAL = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                REQUEST_CONTACT
+            )
+        } else {
+            setContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CONTACT) setContacts()
+    }
+
+    private fun setContacts() {
+        val contactList: ArrayList<Contact> = ArrayList()
+
+        val cursor = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val displayName =
+                    cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+                val number = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                contactList.add(Contact(cursor.getString(displayName), cursor.getString(number)))
+            }
+            cursor.close()
+        }
+
+        val adapter = ContactsAdapter(contactList)
+        val contactRecyclerView = findViewById<RecyclerView>(R.id.rvContacts)
+
+        contactRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        contactRecyclerView.adapter = adapter
+    }
+}
